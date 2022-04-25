b9300ccecbc4068c171574d1c201ed093a95f1e3
Frankllin <frankllin15@hotmail.com>
2022-04-25 16:31:24 -0300

    style: created icons animation

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,6 +5,11 @@ import { request } from "../lib/datocms";
 import { responsiveImageFragment } from "../lib/fragments";
 import { Project } from "../types";
 import { ProjectEmbed } from "./projects/ProjectEmbed";
+import { NextIcon } from "./icons/tecnologies/NextIcon";
+import { TypeScriptIcon } from "./icons/tecnologies/TypescriptIcon";
+import { JavaSriptIcon } from "./icons/tecnologies/JavaScriptIcon";
+import { GraphqlIcon } from "./icons/tecnologies/GraphqlIcon";
+import { TaiwindcssIcon } from "./icons/tecnologies/Taiwindcss";
 
 const project = {
   id: "1",
@@ -19,19 +24,85 @@ type Props = {
   project: Project;
 };
 
+type StyledIconProps = {
+  title?: string;
+  href?: string;
+  children: React.ReactNode;
+  className?: string;
+};
+
+const StyledIcon: React.FC<StyledIconProps> = ({
+  title,
+  href,
+  children,
+  className,
+}) => (
+  <a
+    className={"hover:scale-105 duration-200 ease-in-out " + className}
+    href={href}
+    target="_blank"
+    rel="noreferrer"
+  >
+    <i className="cursor-pointer " title={title}>
+      {children}
+    </i>
+  </a>
+);
+
 export const Home: React.FC<Props> = ({ project }) => {
   return (
     <div className="w-full">
-      <section className="flex flex-col justify-between items-center md:items-start md:flex-row-reverse container-padding ">
-        <div className="w-36 h-36 md:w-52 flex-grow-0 shrink-0 md:h-52 flex-auto  border-2 p-1 rounded-full mb-4 md:mb-0 relative">
-          <Image
-            layout="responsive"
-            className="rounded-full"
-            alt="Foto de perfil"
-            src={require("../../public/images/me.webp")}
-          />
+      <section className="flex flex-col justify-between items-center md:items-start md:flex-row-reverse container-padding  ">
+        <div className="flex flex-col items-center">
+          <div className="w-36 h-36 md:w-52 flex-grow-0 shrink-0 md:h-52 flex-aut  border-2 p-1 rounded-full mb-4 md:mb-0 relative">
+            <Image
+              layout="responsive"
+              className="rounded-full"
+              alt="Foto de perfil"
+              src={require("../../public/images/me.webp")}
+            />
+          </div>
+          <div className="flex items-center gap-2 justify-evenly bg-neutral-100 dark:bg-neutral-700 px-4 rounded-md my-4">
+            <StyledIcon
+              className="animate-[wave_1s]"
+              title="Javascript"
+              href="https://developer.mozilla.org/docs/Web/JavaScript"
+            >
+              <JavaSriptIcon className="w-8 h-8" />
+            </StyledIcon>
+
+            <StyledIcon
+              className="animate-[wave_1s_0.2s]"
+              title="Typescript"
+              href="https://www.typescriptlang.org/"
+            >
+              <TypeScriptIcon className="w-8 h-8" />
+            </StyledIcon>
+            <StyledIcon
+              className="animate-[wave_1s_0.3s]"
+              title="Next.js"
+              href="https://nextjs.org/"
+            >
+              <NextIcon className="w-14 h-14 dark:fill-white" />
+            </StyledIcon>
+            <StyledIcon
+              className="animate-[wave_1s_0.4s]"
+              title="GraphQL"
+              href="https://graphql.org/"
+            >
+              <GraphqlIcon className="w-8 h-8" />
+            </StyledIcon>
+            <StyledIcon
+              className="animate-[wave_1s_0.5s]"
+              title="Taiwindcss"
+              href="https://tailwindcss.com/"
+            >
+              <TaiwindcssIcon className="w-8 h-8" />
+            </StyledIcon>
+          </div>
         </div>
-        <div className="md:mr-8">
+
+        <div className="md:mr-8 animate-fade-in">
           <h1 className="text-2xl mb-2 text-center md:text-left md:text-4xl">
             Olá, eu sou o Frankllin, um desenvolvedor web.
           </h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,32 @@ module.exports = {
           max: "767px",
         },
       },
+      keyframes: {
+        "fade-in": {
+          "0%": {
+            opacity: 0,
+          },
+          "100%": {
+            opacity: 1,
+          },
+        },
+        wave: {
+          "0%": {
+            transform: "translateY(0)",
+          },
+          "50%": {
+            transform: "translateY(-30%)",
+          },
+          "100%": {
+            transform: "translateY(0)",
+            "animation-timing-function": "ease-in_out",
+          },
+        },
+      },
+      animation: {
+        "fade-in": "fade-in 1.5s ease-in-out",
+        wave: "wave 1s ease-in-out",
+      },
     },
   },
   plugins: [],
