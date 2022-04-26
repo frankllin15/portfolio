bb6ac64176d618daae327e21693d37efb2540bd7
Frankllin <frankllin15@hotmail.com>
2022-04-25 21:31:10 -0300

    feat: added seo og-image

--- next-seo.config.js
+++ next-seo.config.js
@@ -6,6 +6,15 @@ const config = {
     locale: "pt_BR",
     url: "https://frankllin.dev",
     site_name: "Frankllin Teixeira",
+    images: [
+      {
+        url: "https://frankllin.dev/static/images/og-image.png",
+        width: 756,
+        height: 621,
+        alt: "Frankllin Teixeira",
+        type: "image/png",
+      },
+    ],
   },
 };
 
