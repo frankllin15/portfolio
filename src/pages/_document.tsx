db3d8f97d9ba04000be7cfdac095fa50a51141db
Frankllin <frankllin15@hotmail.com>
2022-04-27 13:58:17 -0300

    feat: added seo tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,6 +17,23 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@300;500;600&display=swap"
             rel="stylesheet"
           />
+
+          {/* Twitter Meta Tags */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="twitter:domain" content="frankllin.vercel.app" />
+          <meta
+            property="twitter:url"
+            content="https://frankllin.vercel.app/"
+          />
+          <meta name="twitter:title" content="Frankllin Teixeira" />
+          <meta
+            name="twitter:description"
+            content="Um desenvolvedor de software. Conheça mais sobre mim."
+          />
+          <meta
+            name="twitter:image"
+            content="https://frankllin.vercel.app/images/og-image.webp"
+          />
         </Head>
         <body>
           <Main />
